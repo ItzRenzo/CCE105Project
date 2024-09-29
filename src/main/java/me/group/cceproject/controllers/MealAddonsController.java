db81fcc7bc99dc850abcf25e7f43c14cf7826eb8
ItzRenzo <6thgen15@gmail.com>
2024-09-30 00:51:13 +0800

    Forgot

--- src/main/java/me/group/cceproject/controllers/MealAddonsController.java
+++ src/main/java/me/group/cceproject/controllers/MealAddonsController.java
@@ -3,7 +3,7 @@ package me.group.cceproject.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 
-public class BurgerController {
+public class MealAddonsController {
 
     @FXML
     void CheeseBurgerClicked(MouseEvent event) {
