c6bee1932d26d7345ec42eaae03ca1a00161fc1c
fellb <careruffie@gmail.com>
2024-09-30 23:09:59 +0800

    connected order to addons

--- src/main/java/me/group/cceproject/controllers/MealAddonsController.java
+++ src/main/java/me/group/cceproject/controllers/MealAddonsController.java
@@ -19,4 +19,15 @@ public class MealAddonsController {
     void WhopperClicked(MouseEvent event) {
         System.out.println("Whopper clicked");
     }
+
+    private String Addons;
+
+
+    public void setAddons(String addons) {
+        this.Addons = Addons;
+    }
+
+    public String getAddons() {
+        return Addons;
+    }
 }
--- src/main/java/me/group/cceproject/controllers/OrderMenuController.java
+++ src/main/java/me/group/cceproject/controllers/OrderMenuController.java
@@ -1,9 +1,16 @@
 package me.group.cceproject.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class OrderMenuController {
     @FXML
@@ -53,12 +60,31 @@ public class OrderMenuController {
     // Burger Category
     // Yumburger/B1
     @FXML
-    public void B1Clicked(MouseEvent event) {
+    public void B1Clicked(MouseEvent event) { loadMealAddons("Dine In", event);;
     }
 
     // Chicken Wings Category
-    // Spicy CW/C1
+    // Spicy CW/C
     @FXML
-    public void C1Clicked(MouseEvent event) {
+    public void C1Clicked(MouseEvent event) { loadMealAddons("Dine In", event);
+    }
+
+        private void loadMealAddons(String Addons, MouseEvent event) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/me/group/cceproject/MealAddons.fxml"));
+                Parent MealAddonsRoot = loader.load();
+
+                MealAddonsController AddonsController = loader.getController();
+                AddonsController.setAddons(Addons);
+
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Scene MealAddonsScene = new Scene(MealAddonsRoot);
+                stage.setScene(MealAddonsScene);
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Error loading MealAddons.fxml: " + e.getMessage());
+            }
+
     }
 }
